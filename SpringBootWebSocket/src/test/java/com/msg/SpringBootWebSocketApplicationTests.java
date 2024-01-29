7ed4a5d2fe1a6f4050985614c889706afa452040
devendra1707 <pdevendra033@gmail.com>
2024-01-29 11:32:03 +0530

    Spring Boot Web Socket

---
+++ SpringBootWebSocket/src/test/java/com/msg/SpringBootWebSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebSocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
